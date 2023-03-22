bc3add7b93992d1dd9ef9d1665fb657be2ae9ad8
Elseguura <lucassegura2020@gmail.com>
2023-03-22 20:52:16 -0300

    actualizacion

---
+++ src/main/java/com/portfolio/mgb/Dto/dtoHys.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.mgb.Dto;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ *
+ * @author Lucas
+ */
+public class dtoHys {
+    
+    @NotBlank
+    private String nombre;
+    @NotBlank
+    private String porcentaje;
+
+    public dtoHys() {
+    }
+
+    public dtoHys(String nombre, String porcentaje) {
+        this.nombre = nombre;
+        this.porcentaje = porcentaje;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPorcentaje() {
+        return porcentaje;
+    }
+
+    public void setPorcentaje(String porcentaje) {
+        this.porcentaje = porcentaje;
+    }
+    
+    
+}
