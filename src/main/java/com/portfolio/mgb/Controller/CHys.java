e30d0739ba4a801c5865531d3df7bdd12687ae1d
Elseguura <lucassegura2020@gmail.com>
2023-04-14 21:02:54 -0300

    prueba2

--- src/main/java/com/portfolio/mgb/Controller/CHys.java
+++ src/main/java/com/portfolio/mgb/Controller/CHys.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = {"https://argp-frontend.web.app","http://localhost:4200"})
-@RequestMapping("/skill")
+@RequestMapping("/habilidad")
 public class CHys {
     
     @Autowired
